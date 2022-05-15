3e8faf9be7e5e3823a6820a79d6fb041e014392c
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-05-15 17:14:45 -0400

    js

--- super-review/simple-functions/js/main.js
+++ super-review/simple-functions/js/main.js
@@ -11,13 +11,14 @@ function divideThree(n1, n2, n3) {
 console.log(divideThree(100, 2, 2));
 //create a function that multiplys three numbers and returns the product
 function multiThree(n1, n2, n3) {
-  return n1 * n2 * n3;
+  let product = n1 * n2 * n3;
+  return product;
 }
 console.log(multiThree(100, 5, 5));
 //---Medium
 //create a function that takes in three numbers. Add the first two numbers and return the remainder of dividing the sum of the first two numbers by the third number
 function takeThree(n1, n2, n3) {
-  let add = n1 + n2;
+  return (n1 + n2) % n3;
 }
 //---Hard
 //create a function that takes in 4 numbers. Multiply the first two numbers. If the product is greater than 100 add the sum of the last two numbers and console log the value. If the product is less that 100, subtract the difference of the last two numbers and console log the value. If the product is 100, multiply the first three numbers together and alert the remainder of dividing the fourth number
