336887b86a7fd3e1d1441bfb2f2957bbf7253ad0
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-05-15 17:29:32 -0400

    js

--- super-review/temperature-converter/js/main.js
+++ super-review/temperature-converter/js/main.js
@@ -1,1 +1,11 @@
-//Write your pseduo code first! 
+//Write your pseduo code first!
+//find the calculation for celcius and then conver it into feh
+// celcius to ferenheit is ( 9/5 X Celcius) + 32
+//make click event so it only runs on click
+// let celcius = 36;
+// let feh = (9 / 5) * celcius + 32;
+// console.log(feh);
+
+let temp = Number(document.querySelector("input").value);
+temp = (9 / 5) * temp + 32;
+document.querySelector("h2").innerText = temp;
