fd48d9b64a8fc422824464190d282bfa5a7a6a05
flgirl813 <59990889+flgirl813@users.noreply.github.com>
2022-05-22 22:49:50 -0400

    async

--- class35-materials/api-template/js/main.js
+++ class35-materials/api-template/js/main.js
@@ -1,1 +1,23 @@
-//Make an api request using async await
+// //Make an api request using async await
+// function helloWorld() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve("Hello World!");
+//     }, 2000);
+//   });
+// }
+
+// const msg = async function () {
+//   //Async Function Expression
+//   const msg = await helloWorld();
+//   console.log("Message:", msg);
+// };
+
+// const msg1 = async () => {
+//   //Async Arrow Function
+//   const msg = await helloWorld();
+//   console.log("Message:", msg);
+// };
+
+// msg(); // Message: Hello World! <-- after 2 seconds
+// msg1(); // Message: Hello World! <-- after 2 seconds
